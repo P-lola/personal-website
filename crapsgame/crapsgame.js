51a6ae800fa818a92a362b005a578583b03fe4b2
Ilesanmi Pipelola <pipelolailesanmi@gmail.com>
2025-08-31 17:44:50 +0100

    added usernme validation

--- crapsgame/crapsgame.js
+++ crapsgame/crapsgame.js
@@ -6,9 +6,17 @@ const CrapsMainSection = "craps-main-section"
 
 function registerCrapsPlayer() {
     let crapsUsername = (document.getElementById(crapsUsernameInput).value)
-    alert("Got: " + crapsUsername)
+
+    //Validate Username
+    let firstCharIsDigitRegex = /^[0-9]|[^a-zA-Z0-9_]/g
+    if (crapsUsername.length < 4 || firstCharIsDigitRegex.test(crapsUsername)) {
+        alert("Username must be at least 4 characters long, alphanumeric and underscore characters only, no spaces, and cannot start with a number")
+    }
+    else {
     removeregisterPane()
     showMainGamePane()
+    }
+
 }
 function removeregisterPane() {
     document.getElementById(CrapsGameRegisterPane).style.display = "none"
