91de51d408292b90907aea19a2156918fa8ae27d
Ilesanmi Pipelola <pipelolailesanmi@gmail.com>
2025-09-24 23:57:09 +0100

    converted requests to use async await

--- randomquotegenerator/randomquotegenerator.js
+++ randomquotegenerator/randomquotegenerator.js
@@ -3,7 +3,6 @@ const colorPalette = [
     ["#E6BE8A", "#A97142"],
     ["#C5A880", "#7A5C32"],
     ["#D4AF37", "#8B7500"],
-    ["#5A3E36", "#3E2723"],
     ["#8b451390", "#5C3317"],
     ["#D2B48C", "#8B7355"],
     ["#4B3832", "#2F1B12"],
@@ -17,25 +16,18 @@ function getRandomColorCombo() {
     return colorPalette[randomIndex];
 }
 
-function getNewRandomQuote() {
-  fetch('http://api.quotable.io/quotes/random')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then(data => {
-    const colorCombo = getRandomColorCombo()
-    document.getElementById('random-quote-generator').style.background = 'linear-gradient(45deg, ' + colorCombo[0] + ',' + colorCombo[1]
-    const quoteText = data[0].content
-    const quoteAuthor = data[0].author
-    document.getElementById('random-quote-text').innerHTML = quoteText
-    document.getElementById('random-quote-author').innerHTML = quoteAuthor
+async function getNewRandomQuote() {
 
-  })
-  .catch(error => {
-    alert('Error fetching new quote!!');
-  });
-
+  const response = await fetch('http://api.quotable.io/quotes/random')
+  if (!response.ok) {
+    alert('There was a problem getting a new quote')
+  }
+  const data = await response.json()
+  
+  const quoteText = data[0].content
+  const quoteAuthor = data[0].author
+  document.getElementById('random-quote-text').innerHTML = quoteText
+  
+  const colorCombo = getRandomColorCombo()
+  document.getElementById('random-quote-generator').style.background = 'linear-gradient(45deg, ' + colorCombo[0] + ',' + colorCombo[1]
 }
