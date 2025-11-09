03844cf2cf6b26d1e5dda51bbe4bc59bb26861b1
Ilesanmi Pipelola <pipelolailesanmi@gmail.com>
2025-11-09 22:44:22 +0100

    Aded stock analysis dashboard basic workflow

---
+++ stockAnalysisDashboard/stockAnalysisDashboard.js
@@ -0,0 +1,21 @@
+const stockAnalysisDashboardInput = "stock-analysis-dashboard-input"
+
+async function analyzeStock () {
+  document.getElementById('stock-analysis-dashboard-data').innerHTML = ''
+  const stockSymbolToAnalyze = document.getElementById(stockAnalysisDashboardInput).value
+  if (stockSymbolToAnalyze.lenght == 0){
+    alert('You must put in a ticker symbol before running')
+    return
+  }
+  const url = 'http://127.0.0.1:5000/analyze-stock/' + stockSymbolToAnalyze
+
+  console.log("Running...")
+  const response = await fetch(url)
+  if (!response.ok) {
+    alert('There was a problem getting the analysis for ' + stockSymbolToAnalyze)
+    return 
+  }
+  const data = await response.json()
+  console.log(data)
+  document.getElementById('stock-analysis-dashboard-data').innerHTML = JSON.stringify(data)
+}
