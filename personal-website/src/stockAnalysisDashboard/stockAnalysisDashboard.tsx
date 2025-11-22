bad913d72c42fece199584c0ab4e9ce1114a0eac
Ilesanmi Pipelola <pipelolailesanmi@gmail.com>
2025-11-22 02:23:18 +0100

    Added styled components to align contents properly

--- personal-website/src/stockAnalysisDashboard/stockAnalysisDashboard.tsx
+++ personal-website/src/stockAnalysisDashboard/stockAnalysisDashboard.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import { analyzeStock } from "./stockAnalysisDashboard"
+import { analyzeStock, VerticalAlignContainer, VerticalAlignContent } from "./stockAnalysisDashboard"
 import { MutatingDots } from 'react-loader-spinner'
+import './stockAnalysisDashboard.css'
 
 function StockAnalysisDashboard() {
   
@@ -29,48 +30,52 @@ function StockAnalysisDashboard() {
 
   if (gotData) {
     return (
-      <div>
+      <VerticalAlignContainer>
+       <VerticalAlignContent>
         <div onClick={() => goBack()}>BACK</div>
         <div>
           {JSON.stringify(stockData)}
         </div>
-      </div>
+       </VerticalAlignContent>
+      </VerticalAlignContainer>
     )
   }
 
 
   return (
-    <>
-      <div>
-        <div id="stock-analysis-dashboard-title">STOCK ANALYSIS DASHBOARD</div>
-        {isLoading ? (
+    <VerticalAlignContainer>
+      <VerticalAlignContent>
         <div>
-          <MutatingDots
-            visible={true}
-            height="100"
-            width="100"
-            color="#4fa94d"
-            secondaryColor="#4fa94d"
-            radius="12.5"
-            ariaLabel="mutating-dots-loading"
-            wrapperStyle={{}}
-            wrapperClass=""
-          />
+          <div id="stock-analysis-dashboard-title">STOCK ANALYSIS DASHBOARD</div>
+          {isLoading ? (
+          <div>
+            <MutatingDots
+              visible={true}
+              height="100"
+              width="100"
+              color="#4fa94d"
+              secondaryColor="#4fa94d"
+              radius="12.5"
+              ariaLabel="mutating-dots-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+            />
+          </div>
+        ) : (
+          <div>
+            <div id="stock-analysis-dashboard-subtitle">
+              Put in a stock symbol you'd like to analyze (e.g. MSFT)
+          </div>
+            <input
+            value={stockSymbol}
+            onChange={e => setStockSymbol(e.target.value)}>
+            </input>
+            <button className="stock-analysis-dashboard-button" onClick={() => runStockAnalysis()}>Analyze</button>
+          </div>
+        )}
         </div>
-      ) : (
-        <div>
-          <div id="stock-analysis-dashboard-subtitle">
-            Put in a stock symbol you'd like to analyze (e.g. MSFT)
-         </div>
-          <input
-          value={stockSymbol}
-          onChange={e => setStockSymbol(e.target.value)}>
-          </input>
-          <button className="stock-analysis-dashboard-button" onClick={() => runStockAnalysis()}>Analyze</button>
-        </div>
-      )}
-      </div>
-    </>
+      </VerticalAlignContent>
+    </VerticalAlignContainer>
   )
 }
 
